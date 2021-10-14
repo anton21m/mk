b3bb9e2c8dffd47382d4e98e863da0c7f21a9f23
a.shumeyko <a.shumeyko@safenetpay.com>
2021-10-14 12:11:42 +0500

    Task #0

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("success!!")
+
+const player1 = {
+    name: 'player1',
+    hp: 100,
+    img: '',
+    weapon: ['saw', 'toothpick'],
+    attack: function() {
+        console.log(this.name + " Fight...");
+    }
+};
+
+const player2 = {
+    name: 'player2',
+    hp: 100,
+    img: '',
+    weapon: ['saw', 'toothpick'],
+    attack: function() {
+        console.log(this.name + " Fight...");
+    }
+};
